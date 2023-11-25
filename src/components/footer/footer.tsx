e5e64b03344271f372c5beec3ce037216e028d86
Utsav1999 <utsavakash123@gmail.com>
2023-11-25 21:25:54 +0530

    Header & Footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer>
-            
+        <footer className="bg-purple-500/10 text-blue-500/100 p-4 flex justify-center">
+            <p>All Rights Reserved &copy; Vicinity Watcher 2023</p>
         </footer>
     );
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
-        <header>
-            
+        <header className="bg-purple-500/10 text-blue-500/100 p-4 flex justify-between item-center">
+            <Link  href="/" className="cursor-pointer">
+                <p className="font-extrabold text-left text-2xl">Vicinity Watcher</p>
+            </Link>
+            <nav>
+                <ul className="flex space-x-4 cursor-pointer">
+                    <Link href="/log-incident" className="hover:text-purple-800"><li>Log Incident</li></Link>
+                    <Link href="/register-service" className="hover:text-purple-800"><li>Request Service</li></Link>
+                    <Link href="/register" className="hover:text-purple-800"><li>Register/LogIn</li></Link>
+                    <Link href="/weather" className="hover:text-purple-800"><li>Live Weather</li></Link>
+                </ul>
+            </nav>
         </header>
     );
 }
