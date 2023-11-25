00f847d4f3ae2131732626b7b52cf19106246876
Utsav1999 <utsavakash123@gmail.com>
2023-11-25 20:39:10 +0530

    Initial Project Setup

---
+++ app/log-incident/incident.tsx
@@ -0,0 +1,7 @@
+export default function IncidentLog() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
---
+++ app/register-service/page.tsx
@@ -0,0 +1,5 @@
+export default function ServiceLog() {
+    return (
+        <div></div>
+    );
+}
